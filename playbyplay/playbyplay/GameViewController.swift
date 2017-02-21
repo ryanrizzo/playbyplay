34a295cf08c296e6dd6f2ffc58311de0ea01c89b
Ryan Rizzo <ryarriz@gmail.com>
2017-02-20 23:37:39 -0500

    Backgrounds & logo:

---
+++ playbyplay/playbyplay/GameViewController.swift
@@ -0,0 +1,35 @@
+//
+//  GameViewController.swift
+//  playbyplay
+//
+//  Created by Ryan Rizzo on 2/20/17.
+//  Copyright © 2017 Ryan Rizzo. All rights reserved.
+//
+
+import UIKit
+
+class GameViewController: ViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
